f5e1726e9ed7c16470bffbde0be88629f59b0a88
waifuwars <preliator30@gmail.com>
2021-11-04 08:11:01 +0000

    initial

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ image_array = [
     '4.jpg'
 ]
 
+images_array = [
+    '1.png',
+]
+
 function get_random_image() {
     random_index = Math.floor(Math.random() * image_array.length);
 
     selected_image = image_array[random_index];
 
-    document.getElementById('image_shower').src = `./images/${selected_image}`
+    document.getElementById('image_shower').src = `./images/jpeg/${selected_image}`
 }
 
 function get_image() {
-    random_index = Math.floor(Math.random() * image_array.length);
+    random_index = Math.floor(Math.random() * images_array.length);
 
-    selected_image = image_array[random_index];
+    selected_image = images_array[random_index];
 
-    document.getElementById('image_tap').src = `./images/${selected_image}`
+    document.getElementById('image_tap').src = `./images/png/${selected_image}`
 }
